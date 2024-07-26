6f83a687fcce9ea5493a97e18e63c57308ef60f9
adi <adi30531@gmail.com>
2024-07-26 11:02:46 +0300

    style header and navigation buttons + appTheme file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/Header";
+import AppHeader from "@/components/Header";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{headerShown: true, headerTitle: () => <Header/>}} />
+        <Stack.Screen name="(tabs)" options={{headerShown: true, header: () => <AppHeader/>}} />
       </Stack>
     </>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,37 @@
+import React from 'react';
+import { TouchableOpacity, Image } from 'react-native';
+import { Header } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+import appTheme from '../appTheme';
 
-import { Text, View} from "react-native";
+export default function AppHeader() {
+//        <TouchableOpacity onPress={() => navigation.openDrawer()}>
 
-export default function Header() {
-    return (
-      <View>
-        <Text style={{fontSize: 20}}>Beast Runner</Text>
-      </View>
-    );
-  }
+  return (
+    <Header
+      backgroundColor={appTheme.colors.themeColor}
+      leftComponent={
+        <TouchableOpacity>
+          <Image
+            source={require('../assets/assistent.png')}
+            style={{ width: 40, height: 40 }}
+          />
+        </TouchableOpacity>
+      }
+      centerComponent={{ 
+        text: 'Beast Runner', 
+        style: {  
+          color: appTheme.colors.white, 
+          fontSize: 22, 
+          fontFamily: 'Eras ITC', 
+          fontStyle: 'italic', 
+          fontWeight: 'bold' 
+        } 
+      }}
+      rightComponent={{ 
+        icon: 'menu', 
+        color: appTheme.colors.white 
+      }}
+    />
+  );
+}
