0788b3aa4ce41853072d85c97eef9e7f25d0ccdc
adi <adi30531@gmail.com>
2024-07-26 11:08:53 +0300

    make style object

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
-import { TouchableOpacity, Image } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { Header } from 'react-native-elements';
 import appTheme from '../appTheme';
+
+const styles = StyleSheet.create({
+  title: {
+    color: appTheme.colors.white, 
+    fontSize: 22, 
+    fontFamily: 'Eras ITC', 
+    fontStyle: 'italic', 
+    fontWeight: 'bold' 
+  },
+  notifications: {
+    width: 40,
+    height: 40
+  }
+});
 
 export default function AppHeader() {
 
@@ -12,19 +26,13 @@ export default function AppHeader() {
         <TouchableOpacity>
           <Image
             source={require('../assets/assistent.png')}
-            style={{ width: 40, height: 40 }}
+            style={styles.notifications}
           />
         </TouchableOpacity>
       }
       centerComponent={{ 
         text: 'Beast Runner', 
-        style: {  
-          color: appTheme.colors.white, 
-          fontSize: 22, 
-          fontFamily: 'Eras ITC', 
-          fontStyle: 'italic', 
-          fontWeight: 'bold' 
-        } 
+        style: {...styles.title} 
       }}
       rightComponent={{ 
         icon: 'menu', 
