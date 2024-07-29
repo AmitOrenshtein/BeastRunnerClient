6908ec315a328bce408ecb6f6eb3912076bc3db6
Amit Orenshtein <amit.orenshtein@gmail.com>
2024-07-29 20:43:32 +0300

    Adding the workout screen with real time speed from device

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,27 @@
-import { Text, View, StyleSheet } from "react-native";
+import { router } from "expo-router";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function HomePage() {
     return (
         <View style={styles.container}>
             <Text>This is the home page!</Text>
+            <WorkoutBtn/>
         </View>
     );
 };
+
+function WorkoutBtn() {    
+
+    function onClick() {
+        router.replace('/Workout');
+    }
+
+    return (
+            <TouchableOpacity style={styles.workoutBtn} onPress={onClick}>
+                <Text>Workout</Text>
+            </TouchableOpacity>
+    )
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -14,4 +29,16 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
     },
+
+    workoutBtn: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        height: 60,
+        width: 60,
+        backgroundColor: 'grey',
+        borderRadius: 100
+    }
   });
