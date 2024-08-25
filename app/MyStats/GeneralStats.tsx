3ce044f50bb8a7e664f20008884997a13a89c341
RoyVardi4 <vardiroy4@gmail.com>
2024-08-25 13:26:08 +0300

    charts and statistics

--- app/MyStats/GeneralStats.tsx
+++ app/MyStats/GeneralStats.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import React, { FC } from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { GeneralStatsI } from "./types";
 
-const StatisticsCard = () => {
+const StatisticsCard: FC<GeneralStatsI> = ({
+  totalCompletedDistance,
+  totalCompletedTime,
+  trainingsDone,
+}) => {
   return (
     <View style={styles.container}>
       <View style={styles.statItem}>
-        <Text style={styles.statValue}>54</Text>
+        <Text style={styles.statValue}>{totalCompletedDistance}</Text>
         <Text style={styles.statLabel}>km</Text>
       </View>
       <View style={styles.statItem}>
-        <Text style={styles.statValue}>22:45</Text>
-        <Text style={styles.statLabel}>hours</Text>
+        <Text style={styles.statValue}>{totalCompletedTime}</Text>
+        <Text style={styles.statLabel}>minutes</Text>
       </View>
       <View style={styles.statItem}>
-        <Text style={styles.statValue}>18</Text>
+        <Text style={styles.statValue}>{trainingsDone}</Text>
         <Text style={styles.statLabel}>trainings</Text>
       </View>
     </View>
@@ -23,26 +28,26 @@ const StatisticsCard = () => {
 const styles = StyleSheet.create({
   container: {
     width: Dimensions.get("window").width,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: '#F0F0F0', // Light grey background color
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#F0F0F0", // Light grey background color
     padding: 20,
     borderRadius: 20,
-    borderColor: '#E0E0E0', // Slightly darker border color
+    borderColor: "#E0E0E0", // Slightly darker border color
     borderWidth: 1,
   },
   statItem: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   statValue: {
     fontSize: 32,
-    fontWeight: 'bold',
-    color: '#1E90FF', // DodgerBlue color
+    fontWeight: "bold",
+    color: "#1E90FF", // DodgerBlue color
   },
   statLabel: {
     fontSize: 16,
-    color: '#666666', // Grey color
+    color: "#666666", // Grey color
   },
 });
 
--- app/MyStats/index.tsx
+++ app/MyStats/index.tsx
@@ -2,7 +2,16 @@ import { View, Dimensions, StyleSheet, Text } from "react-native";
 import { LineChart, ProgressChart } from "react-native-chart-kit";
 import GeneralStats from "./GeneralStats";
 import { PlanAPI } from "@/serverAPI/PlanAPI";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useFocusEffect } from "expo-router";
+import moment from "moment";
+import { GeneralStatsI } from "./types";
+
+interface PlanDateI {
+  firstDate: string;
+  lastDate: string;
+  progressPercentage: number;
+}
 
 const chartConfig = {
   backgroundColor: "#e26a00",
@@ -22,70 +31,142 @@ const chartConfig = {
 };
 
 const MyStats = () => {
-  const analyzeWorkoutData = (plan:any) => {
-    const result:any = [];
-  
-    plan.forEach((week:any) => {
-      week.days.forEach((day:any) => {
-        if (day.difficultyFeedback !== undefined && day.completedDistance !== undefined && day.completedTime !== undefined) {
+  const analyzeWorkoutData = (plan: any) => {
+    const result: any = [];
+
+    plan.forEach((week: any) => {
+      week.days.forEach((day: any) => {
+        if (
+          day.difficultyFeedback !== undefined &&
+          day.completedDistance !== undefined &&
+          day.completedTime !== undefined
+        ) {
           result.push({
             date: day.date,
             workout: day.workout,
             difficultyFeedback: day.difficultyFeedback,
             completedDistance: day.completedDistance,
-            completedTime: day.completedTime
+            completedTime: day.completedTime,
           });
         }
       });
     });
-  
+
     return result;
+  };
+
+  const getPlanDates = (plan: any) => {
+    if (!plan || plan.length === 0) return null;
+
+    // Extract the first and last dates from the plan
+    const firstDate = plan[0].days[0].date as string;
+    const lastDate = plan[plan.length - 1].days[
+      plan[plan.length - 1].days.length - 1
+    ].date as string;
+
+    const today = moment();
+    const startDate = moment(firstDate, "YYYY/MM/DD");
+    const endDate = moment(lastDate, "YYYY/MM/DD");
+
+    let progressPercentage;
+
+    // If today's date is before the start date, progress is 0%
+    if (today < startDate) {
+      progressPercentage = 0;
+    } else if (today > endDate) {
+      progressPercentage = 100;
+    } else {
+      const totalDuration = endDate.diff(startDate, "days");
+      const elapsedDuration = today.diff(startDate, "days");
+      progressPercentage = elapsedDuration / totalDuration;
+    }
+
+    return {
+      firstDate,
+      lastDate,
+      progressPercentage,
+    };
+  };
+
+  function getGeneralStats(plan: any) {
+    let totalCompletedDistance = 0;
+    let totalCompletedTime = 0;
+    let trainingsDone = 0;
+
+    plan.forEach((week: any) => {
+      week.days.forEach((day: any) => {
+        if (day.completedDistance && day.completedTime) {
+          totalCompletedDistance += day.completedDistance;
+          totalCompletedTime += day.completedTime;
+          trainingsDone++;
+        }
+      });
+    });
+
+    return {
+      totalCompletedDistance,
+      totalCompletedTime,
+      trainingsDone,
+    };
   }
 
-  const [analyzedData, setAnalyzedData] = useState([])
-  
-  useEffect(() => {
-    PlanAPI.getPlan().then((res) => {
-      const analyzedData = analyzeWorkoutData(res.data.plan);
-      setAnalyzedData(analyzedData);
-    });
-  }, []);
-  return analyzedData?.length > 0 && (
-    <View style={styles.container}>
-      <ProgressChart
-        data={{
-          labels: ["Progress Jan 2023 - Feb 2023"], // optional
-          data: [0.8],
-        }}
-        width={Dimensions.get("window").width}
-        height={100}
-        strokeWidth={10}
-        radius={32}
-        chartConfig={chartConfig}
-        hideLegend={false}
-      />
-      <Text>Difficulty level</Text>
-      <LineChart
-        data={{
-          labels: analyzedData?.map((el:any) => el?.date),//["January", "February", "March", "April", "May", "June"],
-          datasets: [
-            {
-              data: analyzedData?.map((el:any) => el?.difficultyFeedback)//[1, 5, 3, 4, 1, 2],
-            },
-          ],
-        }}
-        width={Dimensions.get("window").width} // from react-native
-        height={220}
-        yAxisInterval={1} // optional, defaults to 1
-        chartConfig={chartConfig}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
-      <GeneralStats />
-    </View>
+  const [analyzedData, setAnalyzedData] = useState([]);
+  const [planDates, setPlanDates] = useState<PlanDateI>();
+  const [generalStats, setGeneralStats] = useState<GeneralStatsI>();
+
+  useFocusEffect(
+    useCallback(() => {
+      PlanAPI.getPlan().then((res) => {
+        const analyzedData = analyzeWorkoutData(res.data.plan);
+        const planDates = getPlanDates(res.data.plan);
+        const generalStats = getGeneralStats(res.data.plan);
+        generalStats && setGeneralStats(generalStats);
+        planDates && setPlanDates(planDates);
+        setAnalyzedData(analyzedData);
+      });
+    }, [])
+  );
+
+  return (
+    analyzedData?.length > 0 && (
+      <View style={styles.container}>
+        <ProgressChart
+          data={{
+            labels: [`${planDates?.firstDate} - ${planDates?.lastDate}`],
+            data: [planDates?.progressPercentage || 0],
+          }}
+          width={Dimensions.get("window").width}
+          height={100}
+          strokeWidth={10}
+          radius={32}
+          chartConfig={chartConfig}
+          hideLegend={false}
+        />
+        <LineChart
+          data={{
+            labels: analyzedData?.map((el: any) => el?.date),
+            datasets: [
+              {
+                data: analyzedData?.map((el: any) => el?.difficultyFeedback),
+              },
+            ],
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          chartConfig={chartConfig}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+        <GeneralStats
+          totalCompletedDistance={generalStats?.totalCompletedDistance}
+          totalCompletedTime={generalStats?.totalCompletedTime}
+          trainingsDone={generalStats?.trainingsDone}
+        />
+      </View>
+    )
   );
 };
 
@@ -94,7 +175,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
-  }
+  },
 });
 
 export default MyStats;
